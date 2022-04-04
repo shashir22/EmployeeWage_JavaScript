8c8d26b4154c6f529821924edc7b2704ae16c04d
Shashikumar <shashirgowdas67@gmail.com>
2022-04-05 00:12:11 +0530

    [Shashi]:Calculate employee wage for hours

--- EmpUC3.js
+++ EmpUC3.js
@@ -1,25 +1,29 @@
-//UC-2 PartTime  or FullTime
+//UC-3 using function get work hours
 //constants
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
-let  empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
+function getWorkingHRs(empCheck)
+{
    switch (empCheck)
   {
      //employee is present and parttime
     case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
+        return PART_TIME_HRS;
         break;
     //employee is present and fulltime
     case IS_FULL_TIME:
-       empHrs = FULL_TIME_HRS;
+       return FULL_TIME_HRS;
        break;
      //employee is absent
     default:
-      empHrs = 0;
+      return 0;
    }
-   let empWage = empHrs * WAGE_PER_HR;
-   console.log( "emp wage :"+empWage);
+}
+//let  empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empHrs = getWorkingHRs(empCheck);
+let empWage = empHrs * WAGE_PER_HR;
+console.log( " empHrs: "+ empHrs + "empwage :"+empWage);
