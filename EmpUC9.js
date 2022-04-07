deb288546597d6f886990896b6469c336cbb52b4
Shashikumar <shashirgowdas67@gmail.com>
2022-04-07 13:56:08 +0530

    [Shashi]: using array function code to implement Employee Wage

--- EmpUC9.js
+++ EmpUC9.js
@@ -132,3 +132,31 @@ console.log("UC 7G - Number of Days Emp Worked: "+empDailyWageArr.reduce(totalDa
  
 console.log("UC8A - Emp Wage Map totalHrs: "+
             Array.from(empDailyWageMap.values()).reduce(totalWages,0));
+
+            //UC 9 Arrow Function
+ 
+const findTotal=(totalVal, dailyVal)=>{
+    return totalVal+dailyVal;
+  }
+   
+  let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+  let totalSalary = empDailyWageArr.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
+   
+  console.log("UC 9A - Emp Wage With Arrow.: "+" Total Hours: "+
+             totalHours+" Total Wages: "+totalSalary);
+     
+  let nonWorkingDays = new Array();
+  let partWorkingDays = new Array();
+  let fullWorkingDays = new Array();
+   
+  empDailyHrsMap.forEach((value,key,map)=>{
+     if(value==8) fullWorkingDays.push(key);
+     else if(value ==4) partWorkingDays.push(key);
+     else nonWorkingDays.push(key);
+  });
+     
+  console.log("Full Working Days: "+fullWorkingDays);
+  console.log("Part Working Days: "+partWorkingDays);
+  console.log("Non Working Days: "+nonWorkingDays);
+  
+   
